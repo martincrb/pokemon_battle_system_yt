d59e17d5d498af5973c198187074aff0eef7d62b
martincrb <martincristobaltdr@gmail.com>
2018-10-12 18:14:14 +0200

    ADD typechart, stab and criticals

--- main.py
+++ main.py
@@ -3,40 +3,40 @@ from models.Battle import *
 from models.Pokemon import *
 #First define pokemons with its stats
 
-pokemon1 = Pokemon("Bulbasaur", 78, "grass", "poison")
-pokemon2 = Pokemon("Charmander", 100, "fire", None)
+pokemon1 = Pokemon("Bulbasaur", 100, 11, 3)
+pokemon2 = Pokemon("Charmander", 100, 9, 1)
 pokemon1.current_hp = 45
 pokemon2.current_hp = 39
 #Stats
 
 pokemon1.baseStats = {
-    HP: 108,
-    ATTACK: 130,
-    DEFENSE: 95,
+    HP: 39,
+    ATTACK: 52,
+    DEFENSE: 43,
     SPATTACK: 80,
-    SPDEFENSE: 85,
-    SPEED: 102
+    SPDEFENSE: 65,
+    SPEED: 65
 }
 
 pokemon1.ev = {
-    HP: 74,
-    ATTACK: 190,
-    DEFENSE: 91,
-    SPATTACK: 48,
-    SPDEFENSE: 84,
-    SPEED: 23
+    HP: 0,
+    ATTACK: 0,
+    DEFENSE: 0,
+    SPATTACK: 0,
+    SPDEFENSE: 0,
+    SPEED: 0
 }
 
 pokemon1.iv = {
-    HP: 24,
-    ATTACK: 12,
-    DEFENSE: 30,
-    SPATTACK: 16,
-    SPDEFENSE: 23,
-    SPEED: 5
+    HP: 21,
+    ATTACK: 21,
+    DEFENSE: 21,
+    SPATTACK: 21,
+    SPDEFENSE: 21,
+    SPEED: 21
 }
 pokemon1.compute_stats()
-print(pokemon1.stats)
+
 pokemon2.baseStats = {
     HP: 39,
     ATTACK: 52,
@@ -63,11 +63,12 @@ pokemon2.iv = {
     SPDEFENSE: 21,
     SPEED: 21
 }
-
-
+pokemon2.compute_stats()
+print(pokemon1.stats)
+print(pokemon2.stats)
 #Attacks
-pokemon1.attacks = [Attack("scratch", "normal", PHYSICAL, 10, 10, 100)]
-pokemon2.attacks = [Attack("scratch", "normal", PHYSICAL, 10, 10, 100)]
+pokemon1.attacks = [Attack("latigo cepa", 11, PHYSICAL, 10, 10, 100)]
+pokemon2.attacks = [Attack("scratch", 0, PHYSICAL, 10, 10, 100)]
 
 
 def ask_command(pokemon):
@@ -75,6 +76,7 @@ def ask_command(pokemon):
     while not command:
         tmp_command = input("What should "+pokemon.name+" do?").split(" ")
         if len(tmp_command) == 2:
+            print(tmp_command)
             try:
                 if tmp_command[0] == DO_ATTACK and 0 <= int(tmp_command[1]) < 4:
                     command = Command({DO_ATTACK: int(tmp_command[1])})
--- models/Battle.py
+++ models/Battle.py
@@ -1,5 +1,5 @@
 from constants import *
-
+import random
 
 class Battle:
 
@@ -34,19 +34,44 @@ class Battle:
             attack2 = self.pokemon2.attacks[command2.action[DO_ATTACK]]
 
         # Execute attacks
-        self.pokemon2.current_hp -= attack1.power
-        self.pokemon1.current_hp -= attack2.power
+        self.pokemon2.current_hp -= self.compute_damage(attack1, self.pokemon1, self.pokemon2)
+        self.pokemon1.current_hp -= self.compute_damage(attack2, self.pokemon2, self.pokemon1)
         self.actual_turn += 1
 
     def print_current_status(self):
         print(self.pokemon1.name + " has " + str(self.pokemon1.current_hp) + " left!")
         print(self.pokemon2.name + " has " + str(self.pokemon2.current_hp) + " left!")
 
-    def compute_damage(attack, pokemon1, pokemon2):
-        pass
+    def compute_damage(self, attack, pokemon1, pokemon2):
+        aux = ((2*pokemon1.level)/5) + 2
+        powerFactor = aux * attack.power
+        if attack.category == PHYSICAL:
+            print("Physical attack!")
+            powerFactor *= (pokemon1.stats[ATTACK]/pokemon2.stats[DEFENSE])
+        else:
+            powerFactor *= (pokemon1.stats[SPATTACK]/pokemon2.stats[SPDEFENSE])
+        damage_without_modifier = powerFactor/50 + 2
+        finalDamage = damage_without_modifier * self.compute_damage_modifier(attack, self.pokemon1, self.pokemon2)
+        print(finalDamage)
+        return finalDamage
 
-    def compute_damage_modifier(attack, pokemon1, pokemon2):
-        pass
+    def compute_damage_modifier(self, attack, pokemon1, pokemon2):
+        #Compute STAB
+        stab = 1
+        if attack.type == pokemon1.type1 or attack.type == pokemon1.type2:
+            print("HAS STAB")
+            stab = 1.5
+        #Compute Type effectiveness
+        effectiveness1 = TYPE_CHART[pokemon2.type1][attack.type]
+        effectiveness2 = TYPE_CHART[pokemon2.type2][attack.type]
+        effectiveness_final = effectiveness1 * effectiveness2
+        
+        #Compute Critical
+        critical = 1
+        if random.random() <= 0.1:
+            print(pokemon1.name, "did a critical attack!!")
+            critical = 1.5
+        return stab * effectiveness_final * critical
     
 class Turn:
 
